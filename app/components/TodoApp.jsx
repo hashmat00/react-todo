5a37c5c86bb72f26d3beaa32d0375488ee36f5fa
hashmat00 <info@hashmat00.com>
2016-07-19 22:28:19 +0000

    prepared files for todo app, and added code to check for files inside component in webpack.fonig.js

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,16 @@
+var React = require("react");
+
+var TodoApp = React.createClass({
+    render: function () {
+        return (
+            
+            <div>
+            <h1>Todo App </h1>
+            </div>
+            
+            );
+    }
+});
+
+
+module.exports = TodoApp;
