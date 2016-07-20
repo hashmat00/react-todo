640c1a8f210ef93a7a55a9d50501ac51295659b9
hashmat00 <info@hashmat00.com>
2016-07-20 16:56:43 +0000

    added todo form

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,32 @@
+var React = require("react");
+
+var AddTodo = React.createClass({
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var todoText = this.refs.todoText.value;
+        
+        if(todoText.length > 0){
+            this.refs.todoText.value = '';
+            this.props.onAddTodo(todoText);
+        }else{
+            this.refs.todoText.focus();
+        }
+    },
+    render: function () {
+        var {id, text} = this.props;
+        return (
+            
+            <div>
+            <form onSubmit={this.handleSubmit}>
+            <input type="text" ref='todoText' placeholder='Enter Todo'/>
+            <button className='button expanded'>Add Todo</button>
+            
+            </form>
+            </div>
+            
+            );
+    }
+});
+
+
+module.exports = AddTodo;
